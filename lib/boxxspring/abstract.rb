39d945c772abb72881978cde94522ff5eb975b06
kristoph <github.com@kristoph.net>
2015-05-08 19:58:17 -0400

    reimplemented abstract class through an internal hash

--- lib/boxxspring/abstract.rb
+++ lib/boxxspring/abstract.rb
@@ -3,23 +3,29 @@ module Boxxspring
   class Abstract
 
     def initialize( attributes = {} )
+      @attributes = {}
       attributes.each_pair do | key, value |
         self.send( key, value )
       end
     end
 
+    def to_hash
+      return @attributes
+    end
+
     def method_missing( method, *arguments, &block )
       result = nil
       if arguments.length == 0 
-        result = instance_variable_get( "@#{method}" ) 
+        result = @attributes[ method.to_sym ] 
         if result.nil? 
           result = Abstract.new
-          instance_variable_set( "@#{method}", result )
+          result.instance_eval( &block ) unless block.nil?
+          @attributes[ method.to_sym ] = result 
         end 
       elsif arguments.length == 1
         method = method.to_s.gsub( /=$/, '' )
         result = arguments[ 0 ]
-        instance_variable_set( "@#{method}", result ) 
+        @attributes[ method.to_sym ] = result 
       else
         result = super 
       end
