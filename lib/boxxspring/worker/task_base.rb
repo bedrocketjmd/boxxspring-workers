47e3717cfc55d3cf8e80b052e8f8eae3d4bb210e
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-10-12 16:30:04 -0400

    Add operation method to task base

--- lib/boxxspring/worker/task_base.rb
+++ lib/boxxspring/worker/task_base.rb
@@ -137,6 +137,13 @@ module Boxxspring
         self.delegate_payload( queue_name, payload )
       end
 
+      protected; def operation(endpoint)
+        Boxxspring::Operation.new(
+          endpoint,
+          Boxxspring::Worker.configuration.api_credentials.to_hash
+        )
+      end
+
     end
 
   end
