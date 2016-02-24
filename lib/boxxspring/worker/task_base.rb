1d0ebc9aea102d26dd2eff4250dac8a67097a446
zerogee00 <david@bedrocket.com>
2016-02-24 08:14:56 -0500

    update with develop;log task

--- lib/boxxspring/worker/task_base.rb
+++ lib/boxxspring/worker/task_base.rb
@@ -74,6 +74,7 @@ module Boxxspring
                     task.first.is_a?(Boxxspring::ForbiddenError)
                 self.logger.error(task.first.message)
               else
+                self.logger.error( task )
                 self.logger.error(
                   "The #{self.human_name} worker is unable to retrieve the " +
                   "task with the id #{task_id}."
@@ -135,6 +136,13 @@ module Boxxspring
           }
         } )
         self.delegate_payload( queue_name, payload )
+      end
+
+      protected; def operation(endpoint)
+        Boxxspring::Operation.new(
+          endpoint,
+          Boxxspring::Worker.configuration.api_credentials.to_hash
+        )
       end
 
     end
