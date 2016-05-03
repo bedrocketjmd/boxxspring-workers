01eb75ab8b12d7aa42a04b8a2a614c9e5d549ec9
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2016-05-03 16:28:17 -0400

    Clean up task base

--- lib/boxxspring/worker/task_base.rb
+++ lib/boxxspring/worker/task_base.rb
@@ -18,7 +18,7 @@ module Boxxspring
           self.class.processor.call( task )
         else
           raise RuntimeError.new( 
-            "The #{self.human_name} worker lacks a task processor"
+            "The #{ self.human_name } lacks a task processor"
           )
         end
       end
@@ -27,56 +27,51 @@ module Boxxspring
       # implementation
 
       protected; def process_payload( payload )
-
         result = true
         type_names = self.task_type_name.blank? ? 
-          nil : 
-          [ self.task_type_name ].flatten
+                       nil : [ self.task_type_name ].flatten
         states = self.task_state.blank? ?
-          nil :
-          [ self.task_state ].flatten
-
+                   nil : [ self.task_state ].flatten
         tasks = payload[ 'tasks' ]
-        if ( tasks.present? && tasks.respond_to?( :each ) ) 
+
+        if tasks.present? && tasks.respond_to?( :each )
           tasks.each do | task |
             task_id = task[ 'id' ]
-            if ( type_names.blank? || type_names.include?( task[ 'type_name' ] ) )
+            if type_names.blank? || type_names.include?( task[ 'type_name' ] )
               task = task_read( task[ 'property_id' ], task_id )
+
               if task.is_a?( Boxxspring::Task )
-                if ( states.blank? || states.include?( task.state ) )
+                if states.blank? || states.include?( task.state )
                   self.logger.info(  
-                    "The task (id: #{task.id}) processing has started."
+                    "Task #{ task.id } processing has started."
                   )
                   begin
                     result = self.process_task( task )
-                    message = "The task (id: #{task.id}) processing has ended"
-                    message += " and the message has been retained." if result == false
-                    self.logger.info(  
-                      message
-                    )
+                    message = "Task #{ task.id } processing has ended"
+                    message += " and the message was retained." if result == false
+                    self.logger.info( message )
                   rescue SignalException, StandardError => error
                     if error.is_a?( SignalException )
                       task_state = 'idle'
-                      task_message = "The task (id: #{task.id}) has restarted."
+                      task_message = "Task #{ task.id } has restarted."
                     else
                       task_state = 'failed'
-                      task_message = "The task (id: #{task.id}) processing has failed."
+                      task_message = "Task #{ task.id } processing has failed."
                     end
-                    task = task_write_state(task,
-                                            task_state,
-                                            task_message)
+                    task = task_write_state( task, task_state, task_message )
                     self.logger.error( error.message )
-                    self.logger.error( error.backtrace.join( "\n" ) )
+                    self.logger.info( error.backtrace.join( "\n" ) ) \
+                      if self.debug_mode?
                     raise error if error.is_a?( SignalException )
                   end
                 end
-              elsif task.is_a?(Array) &&
-                    task.first.is_a?(Boxxspring::ForbiddenError)
-                self.logger.error(task.first.message)
+              elsif task.is_a?( Array ) && task.first.message
+                    task.first.class.include?( Boxxspring::ForbiddenError )
+                self.logger.error( task.first.message )
               else
                 self.logger.error(
-                  "The #{self.human_name} worker is unable to retrieve the " +
-                  "task with the id #{task_id}."
+                  "The #{ self.human_name } is unable to retrieve the " +
+                  "task with the id #{ task_id }."
                 )
               end
             end
@@ -84,14 +79,13 @@ module Boxxspring
         end
 
         result
-      
       end
 
       protected; def task_read( property_id, task_id )
         # why did this not work?
         # self.task_operation( property_id ).where( id: task_id ).read
         Boxxspring::Operation.new( 
-          "/properties/#{property_id}/tasks/#{task_id}",
+          "/properties/#{ property_id }/tasks/#{ task_id }",
           Worker.configuration.api_credentials.to_hash
         ).read      
       end
