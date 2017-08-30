202641f725653de3b744cf9096ddaa0e6c10b995
zerogee00 <david@bedrocket.com>
2017-08-30 17:33:03 -0400

    add get_token

--- lib/boxxspring/worker/configuration.rb
+++ lib/boxxspring/worker/configuration.rb
@@ -32,7 +32,29 @@ module Boxxspring
         configuration.each_pair do | name, value |
           self.send( "@#{name}", value )
         end
-      end  
+      end
+
+      def get_token( options = {} )
+
+        token_details = Unimatrix::Authorization::ClientCredentialsGrant.new(
+          client_id: ENV[ 'KEYMAKER_CLIENT' ],
+          client_secret: ENV[ 'KEYMAKER_SECRET' ]
+        ).request_token( options )
+
+        if token_details.include?( 'error' )
+          puts "ERROR: #{ token_details }"
+        end
+
+        unless token_details.is_a?( Hash )
+          return token_details
+        else
+          token_details.symbolize_keys!
+          token_details.update( expires_in: Time.now + token_details[ :expires_in ] ) if token_details[ :expires_in ]
+          api_credentials.access_token =  token_details[ :access_token ]
+          api_credentials.token_expiry =  token_details[ :expires_in ]
+          return api_credentials
+        end
+      end
 
     end
 
