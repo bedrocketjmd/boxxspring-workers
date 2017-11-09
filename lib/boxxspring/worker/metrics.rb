f4ec7491969beb51f178b91dc0c0e41cd3b173a0
jacksonsouza <jacksonsouza@me.com>
2017-11-09 14:47:42 -0800

    update for new parameter format

--- lib/boxxspring/worker/metrics.rb
+++ lib/boxxspring/worker/metrics.rb
@@ -5,7 +5,8 @@ module Boxxspring
     module Metrics
       
       PERMITTED_METRIC_NAMES = [ "Messages", "Invocations", "Failures", "Errors" ]
-      PERMITTED_METRIC_UNITS = [ "Count", "Duration" ]
+      PERMITTED_METRIC_UNITS = [ :count, :seconds, :megabit ]
+
       
       MUTEX = Mutex.new 
 
@@ -25,59 +26,94 @@ module Boxxspring
         initialize_metrics_count
 
         Thread.new do
-          loop do 
-            sleep(1)
-            
+          loop do             
             unless @metrics[ "empty" ]
-              if MUTEX.lock()
-                begin
-              
-                  client.put_metric_data( {
-                    namespace: 'Unimatrix/Worker',
-                    metric_data: format_metrics( @metrics )
-                  } )
-
-                  @metrics = refresh_metrics_count
-
-                ensure
-                  MUTEX.unlock
-                end
+              MUTEX.synchronize do 
+                metrics_payload = Hash.new( @metrics )
+                @metrics = refresh_metrics_count
               end
+
+              begin
+            
+                client.put_metric_data( {
+                  namespace: 'Unimatrix/Worker',
+                  metric_data: format_metrics( metrics_payload )
+                } )
+
+                @metrics = refresh_metrics_count
+
+              ensure
+              end
+
+              sleep 1 
             end
           end
         end
       end
 
-      def metric ( *args, **keyword_args )
-        Thread.new do
-          begin
-            if MUTEX.lock()
-              
-              @dimensions = keyword_args[ :dimensions ] \
-                if keyword_args.key?( :dimensions )
+      #metric "Invocations", :seconds
+        #one invocation, tracking seconds
+      #metric ( [ "Invocations", 1, :seconds ], [ "Failures", 1 ] )
+        #multiple metrics
+      #metric "Error", 2, :megabits
+        #two megabits as an error point 
 
-              args.each do | metric_hash |
-                name, unit = metric_hash.first
+      #metric (name, optional int, optional unit) - can be array
+      def metric ( *args )
+        block_given? ? metric_with_block : metric_without_block
+      
+        args = [ args ] unless args.first.is_a? Array
 
-                if name.in?( PERMITTED_METRIC_NAMES ) && unit.in?( PERMITTED_METRIC_UNITS )
-                  @metrics[ "empty" ] = false
+        args.each do | m |
+          add_metric_to_hash( parse_metric( args ) )
+        end
+      end
+
+      def metric_with_block
+        #time yield
+      end
+
+      def metric_without_block
+        #validate if unit requires block
+      end
+
+      def parse_metric ( arr ) 
+        name, data, unit = arr.first, 1, :count
+        
+        data = arr[ 2 ] if arr[ 2 ].is_a? Integer 
+        unit = arr[ 3 ] unless arr[ 3 ].nil?
+
+        return name, data, unit
+      end
+
+      def add_metric_to_hash ( arr )
+        name, data, unit = arr
+         
+        begin
+          if MUTEX.lock()
+            args.each do | metric_hash |
+              if name.in?( PERMITTED_METRIC_NAMES ) && unit.in?( PERMITTED_METRIC_UNITS )
+                @metrics[ "empty" ] = false
+                
+                unit = unit.to_s.capitalize 
+                
+                unless data
                   @metrics[ name ][ unit ] = @metrics[ name ][ unit ] + 1 
+                else
+                  @metrics[ name ][ unit ] = data
                 end
-
               end
             end
-          ensure
-            MUTEX.unlock();
-            yield if block_given?  
-
           end
+        ensure
+          MUTEX.unlock();
         end
       end
 
       protected; def refresh_metrics_count
         hash = Hash[ PERMITTED_METRIC_NAMES.map { | name |
           [ name, Hash[ PERMITTED_METRIC_UNITS.map { | unit |
-            [ unit, 0 ]
+            [ unit.to_s.capitalize, 0 ]
            } ] ] 
         } ]
         
